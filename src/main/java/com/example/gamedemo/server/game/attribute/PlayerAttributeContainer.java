f455d93416194219fd7e7bfdd85b68ae09019d8e
junah <1347006821@qq.com>
2019-06-13 11:24:58 +0800

    修改战力计算公式

--- src/main/java/com/example/gamedemo/server/game/attribute/PlayerAttributeContainer.java
+++ src/main/java/com/example/gamedemo/server/game/attribute/PlayerAttributeContainer.java
@@ -3,7 +3,6 @@ package com.example.gamedemo.server.game.attribute;
 import com.example.gamedemo.server.game.attribute.constant.AttributeTypeEnum;
 import com.example.gamedemo.server.game.player.model.Player;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -12,34 +11,27 @@ import java.util.concurrent.ConcurrentMap;
  * @date 2019/6/4
  */
 public class PlayerAttributeContainer extends AbstractAttributeContainer<Player> {
-    public PlayerAttributeContainer(Player player) {
-        super(player);
-    }
+  public PlayerAttributeContainer(Player player) {
+    super(player);
+  }
 
-    /**
-     * 计算玩家战力
-     */
-    @Override
-    public void computeCombatIndex() {
-        long combatIndex = 0;
-        long percentage = 100;
-        ConcurrentMap<AttributeTypeEnum, Attribute> attributeMap = this.getAttributeMap();
-        // TODO 公式 计算战力
-        for (Map.Entry<AttributeTypeEnum, Attribute> entry : attributeMap.entrySet()) {
-            Attribute attribute = entry.getValue();
-            if (attribute == null) {
-                break;
-            }
-            if (attribute.getType().isPercentage()) {
-                percentage += attribute.getValue();
-            } else {
-                combatIndex += attribute.getValue();
-            }
-        }
-        //
-        combatIndex *= percentage / 100;
+  /** 计算玩家战力 */
+  @Override
+  public void computeCombatIndex() {
+    long combatIndex = 0;
+    ConcurrentMap<AttributeTypeEnum, Attribute> attributeMap = this.getAttributeMap();
+    /** 计算公式：战力 = 血量*50 + 魔法值*100 + 攻击力*300 + 防御力*200 */
+    Attribute hpAttribute = attributeMap.get(AttributeTypeEnum.HP);
+    Attribute mpAttribute = attributeMap.get(AttributeTypeEnum.MP);
+    Attribute attackAttribute = attributeMap.get(AttributeTypeEnum.ATTACK);
+    Attribute defenseAttribute = attributeMap.get(AttributeTypeEnum.DEFENSE);
 
-        /** 设置战力 */
-        owner.setCombatIndex(combatIndex);
-    }
+    combatIndex =
+        hpAttribute.getValue() * 50
+            + mpAttribute.getValue() * 100
+            + attackAttribute.getValue() * 300
+            + defenseAttribute.getValue() * 200;
+    /** 设置战力 */
+    owner.setCombatIndex(combatIndex);
+  }
 }
