101ce2bf6d7a441a89d2bc907a826089f25f2e8f
junah <1347006821@qq.com>
2019-07-01 11:25:52 +0800

    新增Command模块

---
+++ src/main/java/com/example/gamedemo/common/executer/Command.java
@@ -0,0 +1,19 @@
+package com.example.gamedemo.common.executer;
+
+/**
+ * @author: wengj
+ * @date: 2019/7/1
+ * @description：指令
+ */
+public interface Command {
+  /** 执行指令 */
+  void doAction();
+
+  /**
+   * 取余，指定线程执行
+   *
+   * @param poolSize
+   * @return
+   */
+  int modIndex(int poolSize);
+}
---
+++ src/main/java/com/example/gamedemo/common/executer/SceneCommand.java
@@ -0,0 +1,31 @@
+package com.example.gamedemo.common.executer;
+
+/**
+ * @author wengj
+ * @description
+ * @date 2019/7/1
+ */
+public class SceneCommand implements Command {
+  /** 场景id */
+  private int sceneId;
+
+  public SceneCommand(int sceneId) {
+    this.sceneId = sceneId;
+  }
+
+  @Override
+  public void doAction() {}
+
+  @Override
+  public int modIndex(int poolSize) {
+    return getSceneId() % poolSize;
+  }
+
+  public int getSceneId() {
+    return sceneId;
+  }
+
+  public void setSceneId(int sceneId) {
+    this.sceneId = sceneId;
+  }
+}
